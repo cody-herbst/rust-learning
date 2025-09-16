18c9e1dc7a1456b93f428797bd9d398f879c611c
Cody Herbst <cyherbst@gmail.com>
2025-09-16 18:35:09 -0400

    finished factorial

--- factorial/src/main.rs
+++ factorial/src/main.rs
@@ -1,14 +1,34 @@
 use std::io;
 
 fn main() {
-    let mut input = String::new();
-    io::stdin().read_line( &mut input ).unwrap();
 
-    let res = input.trim().parse::<u64>();
+    let mut found_num = false;
+    let mut num : u64 = 0;
 
-    match res {
-        Ok(num) => println!("{}", num),
-        Err(_) => println!("Not a valid number. "),
+    while !found_num {
+        println!("Input a number: ");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+
+        let res = input.trim().parse::<u64>();
+
+        match res {
+            Ok(input_n) => {
+                num = input_n;
+                found_num = true;
+            },
+            Err(_) => {
+                println!("Not a valid number. ")
+            },
+        }
     }
+
+    let mut product : u64 = 1;
+
+    for i in 1..=num {
+        product *= i
+    }
+
+    println!("{}", product);
 
 }
