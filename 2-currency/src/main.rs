5aef8897cff40d0c8b3882050f4ca9a0969c92a0
Cody Herbst <cyherbst@gmail.com>
2025-09-24 18:20:12 -0400

    start of project 2

--- 2-currency/src/main.rs
+++ 2-currency/src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
+
+enum Currency {
+    USD,
+    GBP,
+    JPY,
+    KRW
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let mut dir : Option<String> = Option::None;
+    match env::current_dir() {
+        Ok(path) => {
+            let dir_string = path.into_os_string().into_string().unwrap();
+            dir = Some(dir_string);
+        },
+        Err(error) => {}
+    }
+
 }
